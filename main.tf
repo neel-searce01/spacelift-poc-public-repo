b3753836035660d0c23fa62888a02f9679523866
Neel Searce <neel.sanghvi@searce.com>
2025-09-02 23:27:50 +0530

    feat: Add GCE VM and Service Account resources

--- main.tf
+++ main.tf
@@ -26,11 +26,68 @@ provider "google" {
 # "example_bucket" is the local name we give this resource inside our Terraform code.
 resource "google_storage_bucket" "example_bucket" {
   # This is the globally unique name for the storage bucket.
-  name          = "my-unique-spacelift-demo-bucket-12345" # CHANGE THIS to a unique name
+  # IMPORTANT: You must change this name to something unique!
+  name          = "my-unique-spacelift-demo-bucket-98765" 
   location      = var.gcp_region
   force_destroy = true # This allows us to easily delete the bucket later
 
   # Labels are key-value pairs that help you organize your GCP resources.
+  labels = {
+    managed-by = "spacelift"
+    purpose    = "integration-demo"
+  }
+}
+
+# ===================================================================
+# ==== NEW CODE: Create a basic GCP Service Account
+# ===================================================================
+# The "google_service_account" resource type creates a new service account.
+# This is a non-human identity that applications (like our VM) can use to
+# authenticate with other Google Cloud services.
+resource "google_service_account" "example_sa" {
+  # The account_id is a unique identifier for the service account within your project.
+  account_id   = var.service_account_name
+  # The display_name is a user-friendly name shown in the GCP Console.
+  display_name = "Demo Service Account for GCE VM"
+}
+
+# ===================================================================
+# ==== NEW CODE: Create a basic GCE Virtual Machine
+# ===================================================================
+# The "google_compute_instance" resource type creates a new virtual machine.
+resource "google_compute_instance" "example_vm" {
+  # The name of the VM instance.
+  name         = var.vm_instance_name
+  # The machine type (e.g., e2-micro is a small, inexpensive option).
+  machine_type = "e2-micro"
+  # A VM must be created in a specific "zone" within a region (e.g., us-central1-a).
+  zone         = var.gcp_zone
+
+  # This block defines the boot disk for the VM.
+  boot_disk {
+    initialize_params {
+      # This specifies the operating system image to use for the boot disk.
+      # Here, we are using a standard Debian 11 image from Google.
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # This block defines the network interface for the VM.
+  # We are attaching it to the "default" network, which exists in every GCP project.
+  network_interface {
+    network = "default"
+  }
+
+  # This block attaches the service account we created above to this VM.
+  # This allows applications running on the VM to use the identity of the service account.
+  service_account {
+    # We reference the email of the service account created in the previous block.
+    # Terraform automatically understands this dependency.
+    email  = google_service_account.example_sa.email
+    scopes = ["cloud-platform"]
+  }
+
+  # Labels for organization.
   labels = {
     managed-by = "spacelift"
     purpose    = "integration-demo"
--- variables.tf
+++ variables.tf
@@ -12,4 +12,27 @@ variable "gcp_region" {
   default     = "us-central1" # Setting a default value is often helpful.
 }
 
-#temp
+# ===================================================================
+# ==== NEW CODE: Variables for the new resources
+# ===================================================================
+
+# This variable will hold the name for our GCE VM instance.
+variable "vm_instance_name" {
+  type        = string
+  description = "The name for the demo GCE VM instance."
+  default     = "spacelift-demo-vm"
+}
+
+# A VM needs a specific zone (e.g., us-central1-a), not just a region.
+variable "gcp_zone" {
+  type        = string
+  description = "The GCP zone to create the GCE VM in."
+  default     = "us-central1-a" # Defaulting to a zone in the default region.
+}
+
+# This variable will hold the unique ID for our new service account.
+variable "service_account_name" {
+  type        = string
+  description = "The unique account_id for the new service account."
+  default     = "spacelift-demo-sa"
+}
